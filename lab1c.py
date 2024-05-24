d7c93e7fcdb5bfbff88643a5d89740ee559c6486
Sabin Bhujel <sbhujel2@myseneca.ca>
2024-05-24 14:14:51 -0400

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Creating an objct  called name and assigning its value "Isaac"
+name = "Isaac"
+# Creating an object called age and assigning its value 72
+age = 72
+# printing a formatted string that includes the name and age objects
+print(name + " is " + str(age) + " years old!")
+
